4370590c9d8e43f5dee60045cdabe6ac0413d097
Juan Fabrega <juan.fabrega@modop.com>
2017-11-29 05:34:59 -0500

    assignment 6 adding gaussian kernel function. completing search for optimal C and sigma values in model.

--- assignment-6/ex6/dataset3Params.m
+++ assignment-6/ex6/dataset3Params.m
@@ -2,8 +2,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
@@ -15,16 +15,40 @@ sigma = 0.3;
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
 
+% Vectors for different values of C and sigma to try
+CVec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+sigmaVec = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 
+% initializing a matrix for tracking error at each C,sigma combination.
+errors = zeros(8, 8);
 
+% nested for loops will return the error rate for every combination of C and sigma
+% we use existing traning and gaussian kernel functions to get a model based on X y.
+% we then use that model to predit values on a cross-validation set (Xval, yval).
+% lastly each error rate is stored in the errors matrix to identify the best combo.
+for i = 1:length(CVec)
+  for j = 1:length(sigmaVec)
+    model = svmTrain(X, y, CVec(i), @(x1, x2) gaussianKernel(x1, x2, sigmaVec(j)));
+    predictions = svmPredict(model, Xval);
+    errors(i, j) = mean(double(predictions ~= yval));
+  end
+end
+
+% finding the minimum value of errors matrix and returning row/col indices.
+minErrors = min(errors(:));
+[row,col] = find(errors==minErrors);
+
+% mapping C and sigma to corresponding row/col values for minimum error.
+C = CVec(row);
+sigma = sigmaVec(col);
 
 
 
